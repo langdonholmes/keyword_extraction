6e524cd258f38f4acebfa5ce36de28d4082ae876
langdonholmes <langdonholmes@gmail.com>
2022-12-10 23:20:02 +0000

    convert spans including parenthesis
    Keyterms often end in parentheses. It would be preferable to keep them, so let's see if SpaCy can train with trailing parentheses. e.g. "least-preferred coworker (LPC)"

--- scripts/convert.py
+++ scripts/convert.py
@@ -6,6 +6,7 @@ import typer
 import warnings
 from pathlib import Path
 from tqdm.auto import tqdm
+import re
 
 import spacy
 from spacy.tokens import Doc, DocBin
@@ -44,6 +45,7 @@ def to_docbin(docs, output_path):
     
 def get_label(sequence_label: iter, default_label_name='keyterm') -> iter:
     '''If no label name is provided, fill that value in with a default.
+    Assumes that sequence_label is either (st, end) or (st, end, label)
     '''
     if len(sequence_label) == 2:
         st, end = sequence_label
@@ -55,33 +57,38 @@ def get_label(sequence_label: iter, default_label_name='keyterm') -> iter:
 def refine_span(doc, st, end, label):
     '''SpaCy sequence labels cannot have leading or trailing whitespace
     or cross token boundaries.
-    Shrinks sequences to non-whitespace from the left, then from the right.
+    Shrinks sequences and update their indices.
     If a label crosses a token boundary, msg the problem 
     and shrink the sequence to the nearest token boundary.
     '''
     orig_sequence = doc.text[st:end]
-    text = doc.text[st:end]
     
-    while text != text.lstrip() and st < len(text):
-        st += 1
-        text = text[st:end]
-    while text != text.rstrip() and end > 1:
-        end -= 1
-        text = text[st:end] 
-        
-    if text != orig_sequence:
-        warnings.warn(f'{orig_sequence} transformed to {text}')
+    # matches longest sequence that starts and ends with a letter, number, or round bracket.
+    no_leading_trailing_space_or_punct = re.compile(r'[\(a-zA-Z0-9].*[a-zA-Z0-9\)]') 
+    match = re.search(no_leading_trailing_space_or_punct, orig_sequence)
+    new_sequence = match[0] if match else orig_sequence
+
+    if new_sequence != orig_sequence:
+        st = st + match.start() # offset start ind by match start
+        end = end - (len(orig_sequence) - match.end()) # offset end index by (old string length - new string length)        
+        msg = f'Entity {orig_sequence} transformed to {new_sequence} at [{st}, {end}]'
+        warnings.warn(msg)
     
     span = doc.char_span(st, end, label=label, alignment_mode='strict')
+    
     if span is None:
-        msg = f"Document: {doc._.name}\nEntity [{st}, {end}] does not align with token boundaries.\nOriginal entity was '{orig_sequence}'"
-        span = doc.char_span(st, end, label=label, alignment_mode='contract')
-        msg += f"\nSetting entity as '{span}'"
+        msg = f'Entity [{st}, {end}] does not align with token boundaries.\nOriginal entity was "{orig_sequence}"'
+        span = doc.char_span(st, end, label=label, alignment_mode='expand')
+        msg += f'\nSetting entity as "{span}" at [{span.start_char}, {span.end_char}]'
         warnings.warn(msg)
     
     return span
 
 def convert(texts: list[dict]):
+    '''Constructs the training examples for spacy train.
+    Creates a barebones spacy doc with no linguistic annotations (just tokenization) and assigns it a name.
+    Processes the sequence labels and adds them to the doc as entities under doc.ents
+    '''
     for sample in tqdm(texts):
         text = sample['text']
         doc = nlp.make_doc(text)
