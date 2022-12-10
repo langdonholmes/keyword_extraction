4e33ffc6316af33e229aeef6863373c8bb3a57d6
langdonholmes <langdonholmes@gmail.com>
2022-12-10 22:09:37 +0000

    spacy is upset with leading/trailing punctuation/whitespace

--- scripts/convert.py
+++ scripts/convert.py
@@ -5,6 +5,7 @@ import srsly
 import typer
 import warnings
 from pathlib import Path
+from tqdm.auto import tqdm
 
 import spacy
 from spacy.tokens import Doc, DocBin
@@ -41,29 +42,61 @@ def to_docbin(docs, output_path):
         db.add(doc)
     db.to_disk(Path(output_path))
     
-def convert(text_lines: list):
-    for line in text_lines:
-        text = line['text']
+def get_label(sequence_label: iter, default_label_name='keyterm') -> iter:
+    '''If no label name is provided, fill that value in with a default.
+    '''
+    if len(sequence_label) == 2:
+        st, end = sequence_label
+        label = default_label_name
+    else:
+        st, end, label = sequence_label
+    return st, end, label
+
+def refine_span(doc, st, end, label):
+    '''SpaCy sequence labels cannot have leading or trailing whitespace
+    or cross token boundaries.
+    Shrinks sequences to non-whitespace from the left, then from the right.
+    If a label crosses a token boundary, msg the problem 
+    and shrink the sequence to the nearest token boundary.
+    '''
+    orig_sequence = doc.text[st:end]
+    text = doc.text[st:end]
+    
+    while text != text.lstrip() and st < len(text):
+        st += 1
+        text = text[st:end]
+    while text != text.rstrip() and end > 1:
+        end -= 1
+        text = text[st:end] 
+        
+    if text != orig_sequence:
+        warnings.warn(f'{orig_sequence} transformed to {text}')
+    
+    span = doc.char_span(st, end, label=label, alignment_mode='strict')
+    if span is None:
+        msg = f"Document: {doc._.name}\nEntity [{st}, {end}] does not align with token boundaries.\nOriginal entity was '{orig_sequence}'"
+        span = doc.char_span(st, end, label=label, alignment_mode='contract')
+        msg += f"\nSetting entity as '{span}'"
+        warnings.warn(msg)
+    
+    return span
+
+def convert(texts: list[dict]):
+    for sample in tqdm(texts):
+        text = sample['text']
         doc = nlp.make_doc(text)
-        doc._.name = line['name']
+        doc._.name = sample['name']
         ents = []
-        for sequence_label in line['labels']:
-            if len(sequence_label) == 2:
-                start, end = sequence_label
-                label = 'keyterm'
-            else:
-                start, end, label = sequence_label
-            span = doc.char_span(start, end, label=label, alignment_mode='strict')
-            if span is None:
-                msg = f"Document: {line['name']}\nEntity [{start}, {end}] does not align with token boundaries.\nOriginal entity was '{doc.text[start:end]}'"
-                span = doc.char_span(start, end, label=label, alignment_mode='contract')
-                msg += f"\nSetting entity as '{span}'"
-                warnings.warn(msg)
-            ents.append(span)
-        doc.ents = ents
+        
+        for sequence_label in sample['labels']:
+            st, end, label = get_label(sequence_label, default_label_name='keyterm')
+            span = refine_span(doc, st, end, label)
+            if span:
+                ents.append(span)
+
+        if ents:
+            doc.ents = ents
         yield doc
-
-
 
 if __name__ == "__main__":
     typer.run(main)
